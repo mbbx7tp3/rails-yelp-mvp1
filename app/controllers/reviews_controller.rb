e3ad5bffd6eed30e3d72fa86878206e284a62211
Tom Poulter <tom.poulter.1@outlook.com>
2020-01-23 20:32:42 +0000

    got there with the file

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,18 @@
 class ReviewsController < ApplicationController
 before_action :set_restaurant
+
   def new
       @review = Review.new
   end
+
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
@@ -15,8 +20,9 @@ before_action :set_restaurant
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
